a60287b0496edb207be296234f864656fbec8260
YopterMatbEro <yopter2013@gmail.com>
2024-12-13 20:54:53 +0300

    first commit

---
+++ module_14_1.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+# Создание таблицы и её структуры
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+""")
+
+# Удаление таблицы (чтобы начать с нуля, в случае непредвиденных обстоятельств :D )
+# cursor.execute("DROP TABLE Users")
+
+# Наполнение таблицы
+for i in range(1, 11):
+    cursor.execute(
+        "INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+        (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', 1000)
+    )
+
+# Изменение данных в таблице
+for i in range(1, 11, 2):
+    cursor.execute("UPDATE Users SET balance = ? WHERE username = ?", (500, f'User{i}'))
+
+# Удаление данных из таблицы
+for i in range(1, 11, 3):
+    cursor.execute("DELETE FROM Users WHERE username = ?", (f'User{i}',))
+
+# Выборка
+cursor.execute("SELECT * FROM Users WHERE age != 60")
+users = cursor.fetchall()
+
+for user in users:
+    print(f'Имя: {user[1]} | Почта: {user[2]} | Возраст: {user[3]} | Баланс: {user[4]}')
+
+connection.commit()
+connection.close()
